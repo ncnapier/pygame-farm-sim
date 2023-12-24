ea308b61995ce224920d0e44a656a43f33f44a12
Nathaniel C Napier <nattydevs@gmail.com>
2023-12-24 08:54:16 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import pygame
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+player = pygame.Rect((300, 250, 50, 50))
+
+run = True
+while run:
+
+    screen.fill((0,0,0))
+
+    pygame.draw.rect(screen, (255, 0, 0), player)
+
+    key = pygame.key.get_pressed()
+    if key[pygame.K_a] == True:
+        player.move_ip(-1, 0)
+    elif key[pygame.K_d] == True:
+        player.move_ip(1, 0)
+    elif key[pygame.K_w] == True:
+        player.move_ip(0, -1)
+    elif key[pygame.K_s] == True:
+        player.move_ip(0, 1)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
