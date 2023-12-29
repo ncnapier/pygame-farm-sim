d88ef498928437a508c47234a5361bdde964c0af
Nathaniel C Napier <nattydevs@gmail.com>
2023-12-29 16:44:11 -0600

    added collision logic and 3d effect

--- code/level.py
+++ code/level.py
@@ -2,8 +2,10 @@ import pygame
 from settings import *
 from player import Player
 from overlay import Overlay
-from sprites import Generic
+from sprites import Generic, Water, WildFlower, Tree
 from pytmx.util_pygame import load_pygame
+from support import *
+
 
 class Level:
 	def __init__(self):
@@ -13,6 +15,7 @@ class Level:
 
 		# sprite groups
 		self.all_sprites = CameraGroup()
+		self.collision_sprites = pygame.sprite.Group()
 
 		self.setup()
 		self.overlay = Overlay(self.player)
@@ -31,10 +34,30 @@ class Level:
 
 		#fence
 		for x, y, surf in tmx_data.get_layer_by_name('Fence').tiles():
-				Generic((x * TILE_SIZE, y * TILE_SIZE),surf,self.all_sprites,LAYERS['main'])
-		
+				Generic((x * TILE_SIZE, y * TILE_SIZE),surf,[self.all_sprites, self.collision_sprites],LAYERS['main'])
 
-		self.player = Player((640,360), self.all_sprites)
+		#water
+		water_frames = import_folder('graphics/water/')
+		for x, y, surf in tmx_data.get_layer_by_name('Water').tiles():
+			Water((x * TILE_SIZE, y * TILE_SIZE), water_frames, self.all_sprites)
+		
+		#trees
+		for obj in tmx_data.get_layer_by_name('Trees'):
+			Tree((obj.x,obj.y), obj.image, [self.all_sprites, self.collision_sprites], obj.name)
+
+
+		#wildflowers
+		for obj in tmx_data.get_layer_by_name('Decoration'):
+			WildFlower((obj.x,obj.y), obj.image, [self.all_sprites, self.collision_sprites])
+
+		# collion tiles
+		for x, y, surf in tmx_data.get_layer_by_name('Collision').tiles():
+			Generic((x * TILE_SIZE, y * TILE_SIZE), pygame.Surface((TILE_SIZE, TILE_SIZE)), self.collision_sprites)
+
+		# Player
+		for obj in tmx_data.get_layer_by_name('Player'):
+			if obj.name == 'Start':
+				self.player = Player((obj.x,obj.y), self.all_sprites, self.collision_sprites)
 		Generic(
 			pos = (0,0),
 			surf = pygame.image.load('graphics/world/ground.png').convert_alpha(),
@@ -60,7 +83,7 @@ class CameraGroup(pygame.sprite.Group):
 		self.offset.y = player.rect.centery - SCREEN_HEIGHT / 2
 
 		for layer in LAYERS.values():
-			for sprite in self.sprites():
+			for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
 				if sprite.z == layer:
 					offset_rect = sprite.rect.copy()
 					offset_rect.center -= self.offset
