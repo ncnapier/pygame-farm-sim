9197be91b47297bce8a1d96b0da25ef2eee23cc9
Nathaniel C Napier <nattydevs@gmail.com>
2024-01-07 17:35:30 -0600

    Add files via upload

--- code/level.py
+++ code/level.py
@@ -2,9 +2,10 @@ import pygame
 from settings import *
 from player import Player
 from overlay import Overlay
-from sprites import Generic, Water, WildFlower, Tree
+from sprites import Generic, Water, WildFlower, Tree, Interaction
 from pytmx.util_pygame import load_pygame
 from support import *
+from transition import Transition
 
 
 class Level:
@@ -17,9 +18,11 @@ class Level:
 		self.all_sprites = CameraGroup()
 		self.collision_sprites = pygame.sprite.Group()
 		self.tree_sprites = pygame.sprite.Group()
+		self.interaction_sprites = pygame.sprite.Group()
 
 		self.setup()
 		self.overlay = Overlay(self.player)
+		self.transition = Transition(self.reset, self.player)
 
 	def setup(self):
 		tmx_data = load_pygame('data/map.tmx')
@@ -44,7 +47,12 @@ class Level:
 		
 		#trees
 		for obj in tmx_data.get_layer_by_name('Trees'):
-			Tree((obj.x,obj.y), obj.image, [self.all_sprites, self.collision_sprites, self.tree_sprites], obj.name)
+			Tree(
+				pos = (obj.x,obj.y),
+				surf = obj.image,
+				groups = [self.all_sprites, self.collision_sprites, self.tree_sprites],
+				name = obj.name, 
+				player_add = self.player_add)
 
 
 		#wildflowers
@@ -58,13 +66,32 @@ class Level:
 		# Player
 		for obj in tmx_data.get_layer_by_name('Player'):
 			if obj.name == 'Start':
-				self.player = Player((obj.x,obj.y), self.all_sprites, self.collision_sprites, self.tree_sprites)
+				self.player = Player(
+					pos = (obj.x,obj.y),
+					group = self.all_sprites,
+					collision_sprites = self.collision_sprites,
+					tree_sprites = self.tree_sprites,
+					interaction = self.interaction_sprites)
+
+			if obj.name == 'Bed':
+				Interaction((obj.x,obj.y), (obj.width,obj.height), self.interaction_sprites, obj.name)
+
 		Generic(
 			pos = (0,0),
 			surf = pygame.image.load('graphics/world/ground.png').convert_alpha(),
 			groups = self.all_sprites,
 			z = LAYERS['ground'])
-		
+
+	def player_add(self,item):
+		self.player.item_inventory[item] += 1	
+
+	def reset(self):
+
+		# trees
+		for tree in self.tree_sprites.sprites():
+			for apple in tree.apple_sprites.sprites():
+				apple.kill()
+			tree.create_fruit()
 
 	def run(self,dt):
 		self.display_surface.fill('black')
@@ -72,6 +99,9 @@ class Level:
 		self.all_sprites.update(dt)
 
 		self.overlay.display()
+		
+		if self.player.sleep:
+			self.transition.play()
 
 class CameraGroup(pygame.sprite.Group):
 	def __init__(self):
--- code/sprites.py
+++ code/sprites.py
@@ -13,6 +13,11 @@ class Generic(pygame.sprite.Sprite):
         self.hitbox = self.rect.copy().inflate(-self.rect.width *
                                                0.2, -self.rect.height * 0.75)
 
+class Interaction(Generic):
+    def __init__(self, pos, size, groups, name):
+        surf = pygame.Surface(size)
+        super().__init__(pos, surf, groups)
+        self.name = name
 
 class Water(Generic):
     def __init__(self, pos, frames, groups):
@@ -63,7 +68,7 @@ class Particle(Generic):
 
 
 class Tree(Generic):
-    def __init__(self, pos, surf, groups, name):
+    def __init__(self, pos, surf, groups, name, player_add):
         super().__init__(pos, surf, groups)
 
         # tree attributes
@@ -79,6 +84,8 @@ class Tree(Generic):
         self.apple_sprites = pygame.sprite.Group()
         self.create_fruit()
 
+        self.player_add = player_add
+
     def damage(self):
 
         # damaging the tree
@@ -92,6 +99,7 @@ class Tree(Generic):
                 surf=random_apple.image,
                 groups=self.groups()[0],
                 z=LAYERS['fruit'])
+            self.player_add('apple')
             random_apple.kill()
 
     def check_death(self):
@@ -101,6 +109,7 @@ class Tree(Generic):
             self.rect = self.image.get_rect(midbottom=self.rect.midbottom)
             self.hitbox = self.rect.copy().inflate((-10, -self.rect.height * 0.6))
             self.alive = False
+            self.player_add('wood')
 
     def update(self, dt):
         if self.alive:
@@ -114,5 +123,5 @@ class Tree(Generic):
                 Generic(
                     pos=(x, y),
                     surf=self.apple_surf,
-                    groups=[self.apple_sprites, self.groups()[0]],
+                    groups=[self.apple_sprites,self.groups()[0]],
                     z=LAYERS['fruit'])
